c6bf4e74f03702d22ce2ebbff266e67055a118dd
Artur Kawala <kawala.artek@gmail.com>
2017-06-03 17:24:29 +0200

    Działa przejście jasny - ciemnu - jasny. Do optymalizacji.

--- WpfApp1/AnimationViewModel.cs
+++ WpfApp1/AnimationViewModel.cs
@@ -3,16 +3,19 @@ using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using System.Windows.Media.Imaging;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Windows;
 
 namespace WpfApp1
 {
-    public enum AnimationType { SlideFromLeft, SlideFromRight, SlideFromTop, SlideFromBottom, InBox, OutBox };
+    public enum AnimationType { SlideFromLeft, SlideFromRight, SlideFromTop, SlideFromBottom, InBox, OutBox, BrightnessOffOn };
     public class AnimationViewModel : INotifyPropertyChanged
     {
         Thread mainThread;
@@ -97,6 +100,7 @@ namespace WpfApp1
             }
         }
         public BitmapImage Image1Source { get; set; }
+        private BitmapImage Image1SourceOriginal { get; set; }
         //ZIndex - ktory obrazek jest "na wierzchu"
         public int Image1ZIndex { get; set; }
 
@@ -158,6 +162,7 @@ namespace WpfApp1
             }
         }
         public BitmapImage Image2Source { get; set; }
+        private BitmapImage Image2SourceOriginal { get; set; }
         //ZIndes - który obrazek jest "na wierzchu"
         public int Image2ZIndex { get; set; }
 
@@ -255,16 +260,21 @@ namespace WpfApp1
             if (openFileDialog.ShowDialog() == true)
             {
                 Image1Source = new BitmapImage(new Uri(openFileDialog.FileName));
+                Image1SourceOriginal = new BitmapImage(Image1Source.UriSource);
             }
             //Notyfikacja ze zmienil sie obrazek 1
             ChangeProperty("Image1Source");
         }
+
+        
+
         private void LoadImage2()
         {
             OpenFileDialog openFileDialog = new OpenFileDialog();
             if (openFileDialog.ShowDialog() == true)
             {
                 Image2Source = new BitmapImage(new Uri(openFileDialog.FileName));
+                Image2SourceOriginal = new BitmapImage(Image2Source.UriSource);
             }
             //Notyfikacja, ze zmienil sie obrazek 2
             ChangeProperty("Image2Source");
@@ -382,6 +392,9 @@ namespace WpfApp1
                 case AnimationType.OutBox:
                     AnimationOutBox();
                     break;
+                case AnimationType.BrightnessOffOn:
+                    BrightnessOffOn();
+                    break;
                 default:
                     break;
             }
@@ -485,6 +498,42 @@ namespace WpfApp1
             ChangeProperty("HeightImg2");
             ChangeProperty("WidthImg2");
         }
+
+        private void BrightnessOffOn()
+        {
+            //ustawienie kolejnosci obrazkow
+            Image1ZIndex = 0;
+            Image2ZIndex = 1;
+
+            //Obliczenia
+            if(sliderValue <= 15)
+            {
+                int brightness_value = (int)((sliderValue / 15) * 255);
+                WriteableBitmap writeableBitmap = ChangeBrightness(Image1Source, -brightness_value);
+                BitmapImage writeableBitmapConverted = ConvertWriteableBitmapToBitmapImage(writeableBitmap);
+                Image1Source = writeableBitmapConverted;
+            }
+            else
+            {
+                Image1ZIndex = 1;
+                Image2ZIndex = 0;
+                int brightness_value = (int)(((sliderValue - 15) / 15) * 255 - 255);
+                WriteableBitmap writeableBitmap = ChangeBrightness(Image2Source, brightness_value);
+                BitmapImage writeableBitmapConverted = ConvertWriteableBitmapToBitmapImage(writeableBitmap);
+                Image2Source = writeableBitmapConverted;
+            }
+
+
+            //Notyfikacja
+            ChangeProperty("Image1ZIndex");
+            ChangeProperty("Image2ZIndex");
+            ChangeProperty("Image1Source");
+            ChangeProperty("Image2Source");
+
+            //Kopiujemy Image1Source i Image2Source z oryginału, aby nie rozjaśniać w nieskończoność
+            Image1Source = new BitmapImage(Image1SourceOriginal.UriSource);
+            Image2Source = new BitmapImage(Image2SourceOriginal.UriSource);
+        }
         private void SetInitialPositionToImage()
         {
             canvasLeftImg1 = 0;
@@ -519,6 +568,69 @@ namespace WpfApp1
             ChangeProperty("WidthImg2");
         }
 
+
+        //zmiana jasności obrazka przekazanego jako source
+        private WriteableBitmap ChangeBrightness(BitmapImage source, int val)
+        {
+            var myWriteableBitmap = new WriteableBitmap(source.PixelWidth, source.PixelHeight, source.DpiX, source.DpiY, source.Format, null);
+
+
+            int stride = source.PixelWidth * 4;
+            int size = source.PixelHeight * stride;
+            byte[] pixels = new byte[size];
+            source.CopyPixels(pixels, stride, 0);
+
+            for (int x = 0; x < source.PixelWidth; x++)
+            {
+                for (int y = 0; y < source.PixelHeight; y++)
+                {
+                    int index = y * stride + 4 * x;
+
+                    int R = (pixels[index] + val);
+                    int G = (pixels[index + 1] + val);
+                    int B = (pixels[index + 2] + val);
+
+                    if (R > 255) R = 255;
+                    if (G > 255) G = 255;
+                    if (B > 255) B = 255;
+                    if (R < 0) R = 0;
+                    if (G < 0) G = 0;
+                    if (B < 0) B = 0;
+
+                    pixels[index] = Convert.ToByte(R);      //R
+                    pixels[index + 1] = Convert.ToByte(G);  //G
+                    pixels[index + 2] = Convert.ToByte(B);  //B
+                }
+            }
+
+            myWriteableBitmap.WritePixels (
+                new Int32Rect(0, 0, source.PixelWidth, source.PixelHeight),
+                pixels,
+                stride,
+                0 );
+
+            // return the new bitmap
+            return myWriteableBitmap;
+        }
+
+        //konwerter z WriteableBitmap do BitmapImage
+        //konieczny z racji tego, że zapisywać pixele można tylko do WriteableBitmap
+        public BitmapImage ConvertWriteableBitmapToBitmapImage(WriteableBitmap wbm)
+        {
+            BitmapImage bmImage = new BitmapImage();
+            using (MemoryStream stream = new MemoryStream())
+            {
+                PngBitmapEncoder encoder = new PngBitmapEncoder();
+                encoder.Frames.Add(BitmapFrame.Create(wbm));
+                encoder.Save(stream);
+                bmImage.BeginInit();
+                bmImage.CacheOption = BitmapCacheOption.OnLoad;
+                bmImage.StreamSource = stream;
+                bmImage.EndInit();
+                bmImage.Freeze();
+            }
+            return bmImage;
+        }
         #endregion
     }
 }
